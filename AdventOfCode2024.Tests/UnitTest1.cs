212e9704393df81102f24449856ea50199da1b5a
Simon Foster <funkysi1701@gmail.com>
2024-12-01 10:01:30 +0000

    Advent Of Code Day1

---
+++ AdventOfCode2024.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode2024
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Day1Part1Example()
+        {
+            var result = Day1.SolvePart1();
+
+            Assert.Equal(11, result);
+        }
+
+        [Fact]
+        public void Day1Part2Example()
+        {
+            var result = Day1.SolvePart2();
+
+            Assert.Equal(31, result);
+        }
+    }
+}
---
+++ AdventOfCode2024/Day1.cs
@@ -0,0 +1,72 @@
+﻿namespace AdventOfCode2024
+{
+    public static class Day1
+    {
+        public static int SolvePart1()
+        {
+            LoadData(out List<int> leftList, out List<int> rightList);
+            var sortedLeftList = leftList.OrderBy(x => x);
+            var sortedRightList = rightList.OrderBy(x => x);
+
+            if (sortedLeftList.Count() != sortedRightList.Count())
+            {
+                throw new ArgumentException("Lists must have the same length");
+            }
+
+            var result = 0;
+            for (int i = 0; i < sortedLeftList.Count(); i++)
+            {
+                var tmpResult = sortedLeftList.ElementAt(i) - sortedRightList.ElementAt(i);
+                if (tmpResult < 0)
+                {
+                    tmpResult *= -1;
+                }
+
+                result += tmpResult;
+            }
+
+            return result;
+        }
+
+        private static void LoadData(out List<int> leftList, out List<int> rightList)
+        {
+            string line;
+            leftList = new List<int>();
+            rightList = new List<int>();
+            StreamReader sr = new StreamReader("input.txt");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                var chars = line.Split("   ");
+                leftList.Add(int.Parse(chars[0]));
+                rightList.Add(int.Parse(chars[1]));
+
+                line = sr.ReadLine();
+            }
+            sr.Close();
+        }
+
+        public static int SolvePart2()
+        {
+            LoadData(out List<int> leftList, out List<int> rightList);
+            var sortedLeftList = leftList.OrderBy(x => x);
+            var sortedRightList = rightList.OrderBy(x => x);
+
+            if (sortedLeftList.Count() != sortedRightList.Count())
+            {
+                throw new ArgumentException("Lists must have the same length");
+            }
+            var result = 0;
+
+            for (int i = 0; i < sortedLeftList.Count(); i++)
+            {
+                var element = sortedLeftList.ElementAt(i);
+                var count = sortedRightList.Count(x => x == element);
+                var tmpResult = element * count;
+                result += tmpResult;
+            }
+
+            return result;
+        }
+    }
+}
